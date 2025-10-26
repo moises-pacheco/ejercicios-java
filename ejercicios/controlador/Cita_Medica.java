597da9581259b512445bbc1bad6b744888a374ba
moises-pacheco <zmoises.pacheco@gmail.com>
2025-10-26 14:02:43 +0100

    Contexto y análisis del ejercicio de 'Cita médica'

---
+++ ejercicios/controlador/Cita_Medica.java
@@ -0,0 +1,27 @@
+package controlador;
+
+public class Cita_Medica {
+    public static void main(String[] args) {
+        //Programa para gestionar los dos tipos de cita.
+        //Sumar cuantas citas tuvieron cada una y sacar la cuenta de cuánto se generó
+
+        //Funciones del programa:
+        /*
+            1. Al iniciar el programa se iniciará un contador para los diferentes tipos de consultas en: 1.
+            2. Cada ves que se realice una consulta, introducirán un 1 si se trata de análisis de sangre y un 2 si se trata de la prueba del COVID.
+            3. Cada vez que indiquen el tipo de consulta, el programa imprimirá el número de turno siguiente.
+
+            Ejemplo:
+
+            - Pulsa 1, el programa imprime el siguiente número de turno.
+                Análisis - turno 2...
+
+            - Pulsa 2, el programa imprime el siguiente número de turno.
+                Prueba COVID, turno2...
+
+             Cuando terminen las consultas pulsando el 0, el programa terminará e informa de cuántos pacientes ha tenido cada consulta y lo que han facturado.
+
+             El análisis de sangre cuesta 60 euros y la prueba del covid 120.
+         */
+    }
+}
